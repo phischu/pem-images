4842faa89edb5d0518b979f20b09a09a97bb400d
phischu <pschuster@uni-koblenz.de>
2014-02-13 18:15:50 +0100

    general structure

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,30 @@
 module Main where
 
+import Pipes
+import qualified Pipes.Prelude as Pipes
+import Codec.Picture
+import Data.Vector
+import System.Directory
+import System.FilePath
+
+filesInDirectory :: (MonadIO m) => FilePath -> Producer FilePath m ()
+filesInDirectory directorypath = do
+    directorycontents <- liftIO (getDirectoryContents directorypath)
+    each directorycontents >-> Pipes.filterM (liftIO . doesFileExist . (directorypath </>))
+
+loadImage :: FilePath -> IO (Image PixelRGB8)
+loadImage = undefined
+
+chooseChannel ::Image PixelRGB8 -> Image Pixel8
+chooseChannel = undefined
+
+gatherRowData :: Image Pixel8 -> Vector Double
+gatherRowData = undefined
+
+testdirectory :: FilePath
+testdirectory = "data/2008-05/PEEM08050800/"
 
 main :: IO ()
-main = print "hallo"
+main = runEffect (
+    filesInDirectory testdirectory >->
+    Pipes.print)
