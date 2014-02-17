7457f4c53a76c1aa92e0028f15d09b3d09816bf4
phischu <pschuster@uni-koblenz.de>
2014-02-17 14:24:19 +0100

    safe pixel lookup

--- src/Main.hs
+++ src/Main.hs
@@ -31,10 +31,12 @@ chooseChannel ::Image PixelRGB8 -> Image Pixel8
 chooseChannel = extractComponent PlaneRed
 
 gatherRowData :: Image Pixel8 -> Vector Double
-gatherRowData image = Vector.fromList []
+gatherRowData image = Vector.fromList [fromIntegral (fromMaybe 0 (mayPixelAt image 20 20))]
 
-safePixelAt :: Image a -> Int -> Int -> Maybe a
-safePixelAt = undefined
+mayPixelAt :: (Pixel a) => Image a -> Int -> Int -> Maybe a
+mayPixelAt image x y
+    | x < 0 || x >= imageWidth image || y < 0 || y > imageHeight image = Nothing
+    | otherwise = Just (pixelAt image x y)
 
 testdirectory :: FilePath
 testdirectory = "data/2008-05/PEEM08050800/"
