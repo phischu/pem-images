8733c5b0da8a510d4cbcdbc6f776ba1cef4ad795
phischu <pschuster@uni-koblenz.de>
2014-02-20 14:56:49 +0100

    basic fold machinery

--- src/ImageQuery.hs
+++ src/ImageQuery.hs
@@ -4,14 +4,15 @@ import ImageProcessing (valueInPoint)
 
 import Codec.Picture (Image,Pixel8,PixelF)
 
-import Control.Foldl (Fold)
+import Control.Foldl (Fold(Fold),list,premap)
+import Control.Applicative ((<$>),(<*>))
 
 import Data.Vector (Vector)
 import Data.Vector as V (map)
 
 data ImageQuery = ImageQuery {
     tableQueries :: Vector TableQuery,
-    lineQuery :: LineQuery,
+    lineQuery :: Vector LineQuery,
     averageImageQuery :: Bool} deriving Show
     
 data TableQuery =
@@ -29,11 +30,18 @@ data LineQuery =
 
 data ImageQueryResult = ImageQueryResult {
     tableRows :: [Vector Double],
-    lineValues :: [Vector Pixel8],
-    averageImage :: (Maybe (Image PixelF))}
+    lineValues :: [Vector (Vector Pixel8)],
+    averageImage :: Maybe (Image PixelF)}
 
 runImageQuery :: ImageQuery -> Fold (Image Pixel8) ImageQueryResult
-runImageQuery = undefined
+runImageQuery (ImageQuery tablequeries linequeries averageimagequery) =
+    ImageQueryResult <$>
+    tableFold tablequeries <*>
+    lineFold linequeries <*>
+    averageImageFold averageimagequery
+
+tableFold :: Vector TableQuery -> Fold (Image Pixel8) [Vector Double]
+tableFold tablequeries = premap (runTableQueries tablequeries) list
 
 runTableQuery :: TableQuery -> Image Pixel8 -> Double
 runTableQuery (ValueInPoint x y) image = valueInPoint x y image
@@ -41,6 +49,18 @@ runTableQuery (ValueInPoint x y) image = valueInPoint x y image
 runTableQueries :: Vector TableQuery -> Image Pixel8 -> Vector Double
 runTableQueries tablequeries image = V.map (flip runTableQuery image) tablequeries
 
+lineFold :: Vector LineQuery -> Fold (Image Pixel8) [Vector (Vector Pixel8)]
+lineFold linequeries = premap (runLineQueries linequeries) list
+
+runLineQuery :: LineQuery -> Image Pixel8 -> Vector Pixel8
 runLineQuery = undefined
 
+runLineQueries :: Vector LineQuery -> Image Pixel8 -> Vector (Vector Pixel8)
+runLineQueries = undefined
+
+averageImageFold :: Bool -> Fold (Image Pixel8) (Maybe (Image PixelF))
+averageImageFold False = Fold const () (const Nothing)
+averageImageFold True = Fold addImage Nothing id
+
+addImage :: Maybe (Image PixelF) -> Image Pixel8 -> Maybe (Image PixelF)
 addImage = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,23 @@
 module Main where
 
-import ImageLoading (imageSeries,ImageLoadingError)
-import ImageQuery (ImageQuery(ImageQuery),runImageQuery,ImageQueryResult(tableRows))
+import ImageLoading (imageSeries)
+import ImageQuery (
+    ImageQuery(ImageQuery),TableQuery(ValueInPoint),
+    runImageQuery,ImageQueryResult(tableRows))
 
-import Codec.Picture (Image,Pixel8)
-
-import Pipes (Producer,runEffect,(>->),hoist,lift)
+import Pipes ((>->))
 import Control.Foldl (purely)
 import Pipes.Prelude (fold)
 import qualified Pipes.Prelude as Pipes
-import Data.Vector (Vector)
 import Data.Vector as V (fromList)
 import Control.Error (EitherT,runEitherT)
-import Control.Monad.Trans.State (StateT,runStateT)
 
 testdirectory :: FilePath
 testdirectory = "data/2008-05/PEEM08050800/"
 
 testquery :: ImageQuery
-testquery = undefined
+testquery = ImageQuery (V.fromList [ValueInPoint 20 20]) (V.fromList []) False
 
 main :: IO ()
-main = runEitherT (purely fold (runImageQuery testquery) (imageSeries testdirectory)) >>= either print (print . tableRows)
+main = runEitherT (purely fold (runImageQuery testquery) (
+    imageSeries testdirectory >-> Pipes.take 20)) >>= either print (print . tableRows)
