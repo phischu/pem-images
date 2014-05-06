259d4441ce35ee0a884dc9b76babb97a53ba9275
phischu <pschuster@uni-koblenz.de>
2014-05-06 12:33:54 +0200

    add parsec dependency

--- src/ImageQuery/Parser.hs
+++ src/ImageQuery/Parser.hs
@@ -3,7 +3,10 @@ module ImageQuery.Parser where
 import ImageQuery (ImageQueryStatement)
 
 parseImageQueries :: String -> Either String [ImageQueryStatement]
-parseImageQueries = undefined
+parseImageQueries imagequeriesstring = mapM parseImageQuery (lines imagequeriesstring)
+
+parseImageQuery :: String -> Either String ImageQueryStatement
+parseImageQuery = undefined
 
 printImageQueries :: [ImageQueryStatement] -> String
 printImageQueries = undefined
