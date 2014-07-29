34b29794d14b1dcac7765671c7adadedbf7d135c
phischu <pschuster@uni-koblenz.de>
2014-07-29 12:12:29 +0200

    mvc

--- src/GUI.hs
+++ src/GUI.hs
@@ -1,12 +1,30 @@
 module GUI where
 
 import Graphics.UI.WX (
-    start,frameLoadRes)
-import Graphics.UI.WXCore (
-    windowShow)
+    start,close,
+    frame,button,
+    Prop((:=)),set,text,outerSize,sz,
+    on,command,
+    layout,widget)
+
+import MVC (runMVC,Model,View,Controller,Managed,asPipe)
+import Pipes (await,yield)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.State.Strict (get,modify)
+
+import Control.Monad (forever)
+
+model :: Model [String] () [String]
+model = asPipe (forever (do
+    () <- await
+    lift (modify (\xs -> "hallo" : xs))
+    lift get >>= yield))
+
+wx :: Managed (View [String],Controller ())
+wx = undefined {-start (do
+    parentFrame <- frame [text := "Image Processing"]
+    quit <- button parentFrame [text := "Quit", on command := close parentFrame,outerSize := sz 500 500]
+    set parentFrame [layout := widget quit])-}
 
 gui :: IO ()
-gui = start (do
-    f <- frameLoadRes "GUI.xrc" "MainFrame" []
-    windowShow f
-    return ())
+gui = runMVC [] model wx >> return ()
