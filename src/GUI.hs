6c7e8f15860bc5b7622c4f77fa876f91d883e37a
phischu <pschuster@uni-koblenz.de>
2014-07-29 12:47:14 +0200

    basic mvc works

--- src/GUI.hs
+++ src/GUI.hs
@@ -2,12 +2,15 @@ module GUI where
 
 import Graphics.UI.WX (
     start,close,
-    frame,button,
-    Prop((:=)),set,text,outerSize,sz,
+    frame,button,singleListBox,
+    Prop((:=)),set,text,items,
     on,command,
-    layout,widget)
+    layout,widget,row)
 
-import MVC (runMVC,Model,View,Controller,Managed,asPipe)
+import MVC (
+    runMVC,Model,View,Controller,asPipe,
+    Managed,managed,asSink,asInput,
+    spawn,Buffer(Single),atomically,send,recv,forkIO)
 import Pipes (await,yield)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.State.Strict (get,modify)
@@ -21,10 +24,17 @@ model = asPipe (forever (do
     lift get >>= yield))
 
 wx :: Managed (View [String],Controller ())
-wx = undefined {-start (do
-    parentFrame <- frame [text := "Image Processing"]
-    quit <- button parentFrame [text := "Quit", on command := close parentFrame,outerSize := sz 500 500]
-    set parentFrame [layout := widget quit])-}
+wx = managed (\k -> do
+    (buttonOutput,buttonInput) <- spawn Single
+    (listOutput,listInput) <- spawn Single
+    forkIO (start (do
+        parentFrame <- frame [text := "Image Processing"]
+        but <- button parentFrame [text := "GO!", on command := atomically (send buttonOutput ()) >> return ()]
+        lst <- singleListBox parentFrame [items := ["world"]]
+        forkIO (forever (atomically (recv listInput) >>= maybe (return ()) (\xs -> set lst [items := xs])))
+        set parentFrame [layout := row 5 [widget lst,widget but]]))
+    let updateList xs = atomically (send listOutput xs) >> return ()
+    k (asSink updateList,asInput buttonInput))
 
 gui :: IO ()
 gui = runMVC [] model wx >> return ()
