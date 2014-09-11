e9f4aed437be91bfdc8cb1cce0695bb44a14cdec
phischu <pschuster@uni-koblenz.de>
2014-09-11 16:57:23 +0200

    save results in path depending on input

--- src/Run.hs
+++ src/Run.hs
@@ -15,7 +15,8 @@ import Codec.Picture (Pixel8,writeBitmap)
 import Pipes (Consumer,runEffect,(>->),await)
 import qualified Pipes.Prelude as Pipes (mapM)
 
-import Control.Monad (forever,forM_)
+import Control.Monad (forever)
+import Data.Foldable (forM_)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.IO.Class (MonadIO,liftIO)
 import Control.Monad.Trans.State (evalStateT,get,put)
@@ -23,25 +24,47 @@ import Control.Monad.Trans.State (evalStateT,get,put)
 import Control.Error (EitherT,runEitherT,scriptIO,fmapLT)
 
 import System.Directory (createDirectoryIfMissing)
-import System.FilePath ((</>))
+import System.FilePath ((</>),takeBaseName)
 import System.IO (Handle,hPutStrLn,openFile,hClose,IOMode(WriteMode))
 
 import Data.Maybe (listToMaybe)
 
 run :: FilePath -> [ImageQueryStatement] -> IO (Either String ())
 run inputdirectory imagequerystatements = runEitherT (do
-    scriptIO (createDirectoryIfMissing True "result")
-    scriptIO (createDirectoryIfMissing True ("result" </> "intermediateimages"))
-    scriptIO (createDirectoryIfMissing True ("result" </> "histograms"))
-    tablehandle <- scriptIO (openFile csvResultPath WriteMode)
+    let inputbasename = takeBaseName inputdirectory
+        resultsPath = "results" </> inputbasename
+    scriptIO (createDirectoryIfMissing True (resultsPath </> islandImagesPath))
+    scriptIO (createDirectoryIfMissing True (resultsPath </> histogramsPath))
+    tablehandle <- scriptIO (openFile (resultsPath </> tablePath) WriteMode)
     runEffect (
         imageSeries inputdirectory >->
         Pipes.mapM (runImageQueries imagequerystatements) >->
-        consumeResults tablehandle) `onFailure` show
+        consumeResults resultsPath tablehandle) `onFailure` show
     scriptIO (hClose tablehandle))
 
-consumeResults :: (MonadIO m) => Handle -> Consumer ImageQueryResult m r
-consumeResults tablehandle = flip evalStateT (0,Nothing,Nothing) (forever (do
+islandImagesPath :: FilePath
+islandImagesPath = "islandimages"
+
+islandImagePath :: Int -> Int -> FilePath
+islandImagePath i c = "islandimage-" ++ show i ++ "-" ++ show c ++ ".bmp"
+
+histogramsPath :: FilePath
+histogramsPath = "histograms"
+
+histogramPath :: Int -> Int -> FilePath
+histogramPath i c = "histogram-" ++ show i ++ "-" ++ show c ++ ".csv"
+
+averageImagePath :: FilePath
+averageImagePath = "averageimage.bmp"
+
+lineImagePath :: Int -> FilePath
+lineImagePath i = "lineimage-" ++ show i ++ ".bmp"
+
+tablePath :: FilePath
+tablePath = "table.csv"
+
+consumeResults :: (MonadIO m) => FilePath -> Handle -> Consumer ImageQueryResult m r
+consumeResults resultsPath tablehandle = flip evalStateT (0,Nothing,Nothing) (forever (do
 
     imagequeryresult <- lift await
     (n,maybeaverageimage,maybelineimages) <- get
@@ -60,48 +83,33 @@ consumeResults tablehandle = flip evalStateT (0,Nothing,Nothing) (forever (do
     put (n',maybeaverageimage',maybelineimages')
 
     liftIO (do
-        saveIntermediateImages n (_outputImages imagequeryresult)
+        saveIslandImages resultsPath n (_outputImages imagequeryresult)
         saveTableRow tablehandle (_tableRow imagequeryresult)
-        saveAverageImage (finalizeAverageImage maybeaverageimage' n')
-        maybe (return ()) saveLineImages maybelineimages'
-        saveHistograms n (_histograms imagequeryresult))))
+        forM_ (finalizeAverageImage maybeaverageimage' n') (saveAverageImage resultsPath)
+        forM_ maybelineimages' (saveLineImages resultsPath)
+        saveHistograms resultsPath n (_histograms imagequeryresult))))
 
-saveIntermediateImages :: Int -> [Image Pixel8] -> IO ()
-saveIntermediateImages n outputimages = forM_ (zip [0..] outputimages) (\(i,image) -> do
-    writeBitmap (intermediateImagePath i n) (imageToJuicy image))
+saveIslandImages :: FilePath -> Int -> [Image Pixel8] -> IO ()
+saveIslandImages resultsPath n islandimages = forM_ (zip [0..] islandimages) (\(i,islandimage) -> do
+    writeBitmap (resultsPath </> islandImagesPath </> islandImagePath i n) (imageToJuicy islandimage))
 
-intermediateImagePath :: Int -> Int -> FilePath
-intermediateImagePath i c = "result" </> "intermediateimages" </> "thresholded-" ++ show i ++ "-" ++ show c ++ ".bmp"
+saveHistograms :: FilePath -> Int -> [[(Int,Int)]] -> IO ()
+saveHistograms resultsPath i histograms = forM_ (zip [0..] histograms) (\(c,histogram) -> do
+    let histogramRow r v = show r ++ " " ++ show v
+    writeFile (resultsPath </> histogramsPath </> histogramPath i c) (unlines (map (uncurry histogramRow) histogram)))
+
+saveAverageImage :: FilePath -> Image Pixel8 -> IO ()
+saveAverageImage resultsPath = writeBitmap (resultsPath </> averageImagePath) . imageToJuicy
+
+saveLineImages :: FilePath -> [Image Pixel8] -> IO ()
+saveLineImages resultsPath lineimages = forM_ (zip [0..] lineimages) (\(i,lineimage) -> do
+    writeBitmap (resultsPath </> lineImagePath i) (imageToJuicy lineimage))
 
 saveTableRow :: Handle -> [Double] -> IO ()
 saveTableRow tablehandle tablerow = hPutStrLn tablehandle (csvRow tablerow)
 
 csvRow :: [Double] -> String
 csvRow = unwords . map show
-
-csvResultPath :: FilePath
-csvResultPath = "result" </> "table.csv"
-
-saveAverageImage :: Maybe (Image Pixel8) -> IO ()
-saveAverageImage = maybe (return ()) (writeBitmap averageImagePath . imageToJuicy)
-
-averageImagePath :: FilePath
-averageImagePath = "result" </> "averageimage.bmp"
-
-saveLineImages :: [Image Pixel8] -> IO ()
-saveLineImages lineimages = forM_ (zip [0..] lineimages) (\(i,lineimage) -> do
-    writeBitmap (lineImagePath i) (imageToJuicy lineimage))
-
-lineImagePath :: Int -> FilePath
-lineImagePath i = "result" </> "lineimage-" ++ show i ++ ".bmp"
-
-saveHistograms :: Int -> [[(Int,Int)]] -> IO ()
-saveHistograms i histograms = forM_ (zip [0..] histograms) (\(c,histogram) -> do
-    let histogramRow r v = show r ++ " " ++ show v
-    writeFile (histogramPath i c) (unlines (map (uncurry histogramRow) histogram)))
-
-histogramPath :: Int -> Int -> FilePath
-histogramPath i c = "result" </> "histograms" </> "histogram-" ++ show i ++ "-" ++ show c ++ ".csv"
 
 onFailure :: (Monad m) => EitherT a m b -> (a -> c) -> EitherT c m b
 onFailure = flip fmapLT
