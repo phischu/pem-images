2144b3aa3443b33d298952792d412e1116fe3fd9
phischu <pschuster@uni-koblenz.de>
2014-09-15 11:12:39 +0200

    remove incode tests

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,6 @@
 module Main where
 
 import GUI (gui)
-import ImageQuery (
-    ImageQueryStatement(SetImageQueryParameter,GetImageQueryResult),
-    ImageQuery(TableQuery,IslandImage,LineImage,ImageOfAverage),
-    ImageQueryParameter(Threshold),
-    Orientation(Horizontal),
-    TableQuery(..),
-    IslandQuery(..))
-
-testdirectory :: FilePath
-testdirectory = "data/small_dark_islands/"
-
-testqueries :: [ImageQueryStatement]
-testqueries = [
-    SetImageQueryParameter (Threshold 14),
-    GetImageQueryResult (TableQuery (IslandQuery NumberOfIslands)),
-    GetImageQueryResult (TableQuery (IslandQuery  AverageOutlineOfIslands)),
-    GetImageQueryResult IslandImage,
-    SetImageQueryParameter (Threshold 251),
-    GetImageQueryResult IslandImage,
-    GetImageQueryResult (TableQuery (IslandQuery NumberOfIslands)),
-    GetImageQueryResult (TableQuery (AverageAroundPoint 2 126 12)),
-    GetImageQueryResult ImageOfAverage,
-    GetImageQueryResult (LineImage Horizontal 5 6 300)]
-
-differentThresholds :: [ImageQueryStatement]
-differentThresholds = do
-    threshold <- [0,8..255]
-    [SetImageQueryParameter (Threshold threshold),GetImageQueryResult IslandImage]
 
 main :: IO ()
 main = gui
